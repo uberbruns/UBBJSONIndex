c2ca07af8de511d505e907fd0c9484f1ee6e4dac
Karsten Bruns <bruns42@googlemail.com>
2012-10-30 16:07:34 +0100

    code optimization

--- autoindex.php
+++ autoindex.php
@@ -10,9 +10,7 @@ header('Content-Type: application/json');
 
 $list = array();
 $this_dir = dirname(__FILE__);
-$dir = substr($this_dir, strlen($_SERVER['DOCUMENT_ROOT'])) ;
-$requested_dir = substr($_SERVER['REQUEST_URI'], strlen($dir)+1) ;
-$requested_dir = realpath($this_dir . '/' . $requested_dir);
+$requested_dir = realpath($_SERVER['DOCUMENT_ROOT'] . '/' . $_SERVER['REQUEST_URI']);
 
 if (strlen($requested_dir) < strlen($this_dir) || !file_exists($requested_dir)) {
 	header("Status: 404 Not Found");
